979337ce7f11854d0e361238a3a62ade8e2241bc
niubrandon <niubrandon@gmail.com>
2022-04-26 22:22:45 -0400

    added post users

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,12 +11,46 @@ import (
 
 func main() {
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", Hello)
-	http.Handle("/", r)
-	fmt.Println("Starting up on 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	type user struct {
+		ID       int    `json:"id"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
+	var users = []user{
+		{ID: 1, Email: "niubrandon@nebula.com", Password: "superman"},
+		{ID: 2, Email: "admin@nebula.com", Password: "superman"},
+	}
+
+	//TO-DO get OpenAPI
+	r := mux.NewRouter()
+
+	// POST on /users
+	r.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "You are trying to login")
+		var u user
+		//decode the payload
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			http.Error(w, "bad request", 400)
+			return
+		}
+		//check if user is in database
+
+		//send jwt
+
+		// vars := mux.Vars(r)
+		//	email := r.FormValue("email")
+		//	password := r.FormValue("password")
+		//check password
+
+		fmt.Println(u)
+		fmt.Println(users)
+		//send jwt token
+	}).Methods("POST")
+
+	fmt.Println("Starting up on 8080")
+
+	// CORS
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000", "http://foo.com:8080"},
 		AllowCredentials: true,
