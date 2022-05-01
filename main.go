4389eb17a1d8d6cf2084d04898549e471772b4b8
niubrandon <niubrandon@gmail.com>
2022-04-30 23:14:25 -0400

    added post users endpoint

--- main.go
+++ main.go
@@ -52,9 +52,10 @@ func main() {
 	)
 
 	type user struct {
-		ID       int    `json:"id"`
+		Username string `json:"username"`
 		Email    string `json:"email"`
 		Password string `json:"password"`
+		Phone    string `json:"phone"`
 	}
 
 	mySigningKey := []byte("AllYourBase")
@@ -67,9 +68,63 @@ func main() {
 	// TO-DO get OpenAPI Swagger
 	r := mux.NewRouter()
 
+	// POST on /users
+	r.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Post request /users")
+		var u user
+		// decode the payload
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			http.Error(w, "bad request", 400)
+			return
+		}
+		// check if email is in the db
+		var uu user
+		err := db.QueryRow("SELECT email from users WHERE email=$1", u.Email).Scan(&uu.Email)
+		if err != nil {
+			fmt.Println("Ready to create account", err)
+			// can't find account, create new account
+			// encrpt password
+			hashedPwd := hashAndSalt([]byte(u.Password))
+			// add to db
+			result, err := db.Exec("INSERT INTO users (username, email, password, phone) VALUES ($1, $2, $3, $4)", u.Password, u.Email, hashedPwd, u.Phone)
+			if err != nil {
+				http.Error(w, "creating user failed in db", 400)
+				fmt.Println("db execution error", err)
+				return
+			}
+			fmt.Println("New user created:", result)
+			// send jwt token
+			// Create the Claims
+			claims := MyCustomClaims{
+				u.Email,
+				jwt.StandardClaims{
+					ExpiresAt: 15000,
+					Issuer:    "test",
+				},
+			}
+			// generate jwt token
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			ss, err := token.SignedString(mySigningKey)
+			expiresAt := time.Now().Add(1200 * time.Second)
+			fmt.Printf("%v %v", ss, err)
+			// send jwt as cookie
+			http.SetCookie(w, &http.Cookie{
+				Name:    "session_token",
+				Value:   ss,
+				Expires: expiresAt,
+			})
+			return
+		} else {
+
+			http.Error(w, "email exists", 400)
+			return
+		}
+
+	})
+
 	// POST on /login
 	r.HandleFunc("/users/login", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Post request /users")
+		fmt.Println("Post request /users/login")
 		var u user
 		// decode the payload
 		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
